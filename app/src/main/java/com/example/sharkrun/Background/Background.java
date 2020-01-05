0bcb72ecf0b8c12c35a26cca4ec5d0582c149e17
AlanTw <Alanwisny@hotmail.com>
2020-01-05 07:57:21 +0100

    First Commit

---
+++ app/src/main/java/com/example/sharkrun/Background/Background.java
@@ -0,0 +1,21 @@
+package com.example.sharkrun.Background;
+
+import android.graphics.Bitmap;
+
+public class Background {
+
+    private Bitmap image;
+    private int x, y;
+
+    public Background(Bitmap res){
+        image = res;
+    }
+
+    public void update(){
+
+    }
+
+    public void draw(){
+
+    }
+}
